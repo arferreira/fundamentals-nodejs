4e4427c08a4914f7602e3de8ccb9f624230a294f
Antonio Souza <antonio.eschola@gmail.com>
2022-07-22 22:44:43 -0400

    configure nodemon and make endpoing to test api

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
+const { response } = require('express');
 const express = require('express');
 
 const app = express();
 
+let category = {
+  id: '18923012-0128301923-1823012',
+  title: 'Apenas mais uma categoria',
+  description: 'Esta é uma categoria diferente',
+  status: true
+}
 
 app.get('/', (request, response) => {
-  return response.json({
-    'name': 'title'
-  });
+  return response.json({message: 'NodeJS API Rest'});
+});
+
+// Endpoint to Categories list
+
+app.get('/categories', (request, response) => {
+  return response.status(200).json(category);
+});
+
+// Endpoint to get a single category
+app.get('categories/:id', (request, response) => {
+  return response.status(200).json(category);
 });
 
 
